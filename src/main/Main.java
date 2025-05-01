9296781a122a2ba75fbef84df1d48fdc61888473
hunterpowell <161683842+hunterpowell@users.noreply.github.com>
2025-04-30 22:46:40 -0700

    init

--- src/main/Main.java
+++ src/main/Main.java
@@ -29,9 +29,12 @@ public class Main {
 	}
 
 	public static void main(String args[]) {
-		SimulationState state = new SimulationState();
-		initializeSimulation(state);
-		runSimulation(state);
+		// SimulationState state = new SimulationState();
+		// initializeSimulation(state);
+		// runSimulation(state);
+
+		MazeGenerator maze = new MazeGenerator(23);
+		maze.displayMaze();
 	}
 
 	private static void initializeSimulation(SimulationState state) {
